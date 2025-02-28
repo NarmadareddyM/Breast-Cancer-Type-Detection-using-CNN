878f41c25d2ed26c610571a44bb49f2ca4ccfc55
NarmadareddyM <narmadam910@gmail.com>
2025-02-28 13:48:10 +0530

    Update cancer-cnn.py

--- edunet1/cancer-cnn.py
+++ edunet1/cancer-cnn.py
@@ -8,7 +8,9 @@ Original file is located at
 """
 
 # Install necessary libraries
-!pip install streamlit pyngrok opencv-python-headless numpy tensorflow scikit-learn zipfile36
+subprocess.run(["pip", "install", "streamlit", "pyngrok", "opencv-python-headless", "numpy", "tensorflow", "scikit-learn", "zipfile36"], check=True)
+subprocess.run(["pip", "install", "--user", "streamlit", "pyngrok", "opencv-python-headless", "numpy", "tensorflow", "scikit-learn", "zipfile36"], check=True)
+
 
 import streamlit as st
 import cv2
